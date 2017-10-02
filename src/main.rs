712f3dfa33289f1b2354099ace80ba9e0ce1455a
Colin Wallace <wallacoloo@gmail.com>
2017-10-01 21:31:36 -0700

    Handle HTTP errors gracefully (and abort otherwise)

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ extern crate gdax_client;
 use std::time::Duration;
 use std::thread;
 
-use gdax_client::{NewOrder, PrivateClient, Side, SizeOrFunds};
+use gdax_client::{Error, NewOrder, PrivateClient, Side, SizeOrFunds};
 
 /// How many $USD to exchange per transaction
 const AMT_USD: f64 = 0.0228155;
 /// How frequently to transact, in seconds
 const DELAY_SECS: u64 = 60;
+/// How long to wait before retry when experiencing HTTP errors, in seconds
+const HTTP_RETRY_SECS: u64 = 30;
 
 const CB_KEY: &'static str = include_str!("../api.key");
 const CB_SECRET: &'static str = include_str!("../api.secret");
@@ -30,8 +32,22 @@ fn main() {
             print!("{:.10} {}    ", acc.available, acc.currency);
         }
         println!("");
-        println!("Posting market order: {:?} {:?}", order, private_client.post_order(&order));
-        thread::sleep(Duration::from_secs(DELAY_SECS));
+        println!("Posting market order: {:?}", order);
+        match private_client.post_order(&order) {
+            Ok(ok) => {
+                println!("  -> {}", ok);
+                thread::sleep(Duration::from_secs(DELAY_SECS));
+            },
+            Err(Error::Http(err)) => {
+                // Http error; could indicate temporary network outage
+                println!("  -> {:?}", err);
+                thread::sleep(Duration::from_secs(HTTP_RETRY_SECS));
+            },
+            Err(other) => {
+                println!("  -> {:?}", other);
+                panic!("Error posting order: {:?}", other);
+            }
+        }
     }
 
 }
